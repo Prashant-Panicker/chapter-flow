464a8f65a779f0f28d92823b45c07696a8c30361
Prashant Panicker <46787674+Prashant-Panicker@users.noreply.github.com>
2026-08-16 16:43:36 +0530

    fix: restore reader_screen.dart and remove unused _prefetchRawText

--- lib/screens/reader_screen.dart
+++ lib/screens/reader_screen.dart
@@ -1,1 +1,44 @@
-PLACEHOLDER
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+import '../models/chapter.dart';
+import '../services/extraction_js.dart';
+import '../services/js_result.dart';
+import '../services/storage_service.dart';
+import '../services/translation_service.dart';
+import '../theme/app_theme.dart';
+import '../widgets/reader_nav_bar.dart';
+
+enum ReaderViewMode { english, bilingual, source }
+
+class ReaderScreen extends StatefulWidget {
+  const ReaderScreen({
+    super.key,
+    required this.url,
+    required this.pageTitle,
+    required this.bodyText,
+    required this.webViewController,
+    this.bookTitle,
+    this.chapterTitle,
+    this.chapterNumber,
+    this.prevUrl,
+    this.nextUrl,
+    this.tocUrl,
+  });
+
+  final String url;
+  final String pageTitle;
+  final String bodyText;
+  final String? bookTitle;
+  final String? chapterTitle;
+  final String? chapterNumber;
+  final String? prevUrl;
+  final String? nextUrl;
+  final String? tocUrl;
+  final InAppWebViewController webViewController;
+
+  @override
+  State<ReaderScreen> createState() => _ReaderScreenState();
+}
